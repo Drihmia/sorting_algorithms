23256fa11f2d8a72d9f5958b354fe9acf6e1328f
Drihmia <drihmia.redouane@gmail.com>
2023-12-15 10:01:50 +0100

    save this version, is working but not same output as the example given - my funtion sort 1st 2 element the add one and follow it until is sorted with 2 previous ones the add new element(4 now) and insert it until those 4 elemet are sorted then it goes and add 5th elemet ...

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+  * insertion_sort_list - a function that sorts a doubly linked list of
+  * integers in ascending order using the Insertion sort algorithm.
+  * @list: array ok integers.
+  * Return: void.
+  */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp = NULL, *pv = NULL, *h = NULL, *nt = NULL;
+	int i = 0;
+	if (!(list) || !(*list) || !((*list)->next))
+		return;
+	tmp = *list, h = (*list);
+	while (h)
+	{
+		tmp = h;
+		while (tmp)
+		{
+			i++;
+			if (tmp->prev && tmp->prev->n < tmp->n)
+				break;
+			if (tmp->prev && tmp->prev->n > tmp->n)
+			{
+				pv = tmp->prev, nt = tmp->next;
+
+				pv->next = tmp->next;
+
+				if (nt)
+					nt->prev = tmp->prev;
+
+				tmp->prev = pv->prev, tmp->next = pv;
+
+				if (pv->prev == NULL)
+					(*list) = tmp;
+				else
+					pv->prev->next = tmp;
+
+				pv->prev = tmp;
+				print_list(*list);
+			}
+			else
+				tmp = tmp->prev;
+		}
+		h = h->next;
+	}
+	printf("Number of total loops : %d\n", i);
+}
